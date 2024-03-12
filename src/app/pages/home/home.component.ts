068f0e8cfb2bb2381ef99858fc332e7cd12a35dc
Leyton Viner <leytonytube@gmail.com>
2024-03-12 14:13:02 +0000

    Updated colors, fixed HTML indent where needed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,17 @@ import { ProjectsComponent } from '../../components/projects/projects.component'
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css', './home-about.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  constructor(public meta:Meta)
+  {
+
+  }
+
+  ngOnInit():void
+  {
+    this.meta.updateTag({property: 'og:title', content: 'MELTPACK.NET | HOME'});
+    this.meta.updateTag({property: 'og:image', content: 'YOUR_IMAGE'});
+    this.meta.updateTag({property: 'og:url', content: 'https://meltpack.net'})
+  }
 }
